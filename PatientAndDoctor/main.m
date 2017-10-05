29f8082e3ff9893273cd437a2f7e73ed516528a9
Sanjay Shah <Sanjay Shah>
2017-10-05 14:02:49 -0700

    Initial Commit

---
+++ PatientAndDoctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PatientAndDoctor
+//
+//  Created by Sanjay Shah on 2017-10-05.
+//  Copyright © 2017 Sanjay Shah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
